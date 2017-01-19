022a0d77129514e20bba552207d2baede9094e15
Serg Levin <serg9nn@gmail.com>
2017-01-19 16:45:40 +0300

    Tests of GroupedList

--- groupingadapter/src/test/java/ru/ncom/groupingrvadapter/ExampleUnitTest.java
+++ groupingadapter/src/test/java/ru/ncom/groupingrvadapter/ExampleUnitTest.java
@@ -18,10 +18,92 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 
-    @Test
-    public void t1() {
-        GroupedList<String> gl = new GroupedList<>(null);
-        gl.addAll("a","b");
-        assertEquals(2,gl.size());
+    enum Callbacks {
+        onClear, onDataSorted, onGroupedItemAdded, onUngroupedItemsAdded, onHeaderAdded
+        , onHeaderRemoved,onGroupedItemRemoved,onUngroupedItemRemoved
+        ,LAST
     }
+
+    @Test
+    public void testGroupedList01() throws Exception {
+        final int[] callbacksRun = new int[Callbacks.LAST.ordinal()];
+        // sample callback, creates sample comparator for String
+        GroupedList.Callback<String> cbFirstChar = new GroupedList.Callback<String>() {
+            @Override
+            public ComparatorGrouper<String> getComparatorGrouper(String sortField) {
+                return new ComparatorGrouper<String>() {
+                    @Override
+                    public String getSortKey(String m) {
+                        return m.substring(0,1);
+                    }
+                    // no title specified - it's the same as sort key.
+                };
+            }
+
+            // count method calls
+
+            @Override
+            public void onClear() {
+                callbacksRun[Callbacks.onClear.ordinal()]++;
+            }
+
+            @Override
+            public void onDataSorted(GroupedList<String> gl) {
+                callbacksRun[Callbacks.onDataSorted.ordinal()]++;
+            }
+
+            @Override
+            public void onGroupedItemAdded(int hpos, String item, int pos) {
+                callbacksRun[Callbacks.onGroupedItemAdded.ordinal()]++;
+            }
+
+            @Override
+            public void onUngroupedItemsAdded(List<String> items) {
+                callbacksRun[Callbacks.onUngroupedItemsAdded.ordinal()]++;
+            }
+
+            @Override
+            public void onHeaderAdded(Header<String> h, int pos) {
+                // Fires only at add(), addAll() calls onDataSorted()
+                callbacksRun[Callbacks.onHeaderAdded.ordinal()]++;
+            }
+
+            @Override
+            public void onHeaderRemoved(int hpos) {
+                callbacksRun[Callbacks.onHeaderRemoved.ordinal()]++;
+            }
+
+            @Override
+            public void onGroupedItemRemoved(int hpos, int tpos) {
+                callbacksRun[Callbacks.onGroupedItemRemoved.ordinal()]++;
+            }
+
+            @Override
+            public void onUngroupedItemRemoved(int tpos) {
+                callbacksRun[Callbacks.onUngroupedItemRemoved.ordinal()]++;
+            }
+        };
+
+        GroupedList<String> gl = new GroupedList<>(cbFirstChar);
+        gl.addAll("ab","bc");
+        assertEquals(2,gl.size());
+        gl.add("ax");
+        gl.add("ax");
+        gl.remove("ab");
+        assertEquals(3,gl.size());
+        // no matter what field, cbFirstChar only implements sort/group by 1st char of the string
+        // doesn't call callback
+        gl.doSort("");
+        assertEquals(0,callbacksRun[Callbacks.onDataSorted.ordinal()]);
+
+        // calls callback
+        gl.sort("");
+        assertEquals(1,callbacksRun[Callbacks.onDataSorted.ordinal()]);
+        assertEquals(2,gl.getHeaders().size());
+
+        gl.remove("ax");
+        assertEquals(1,callbacksRun[Callbacks.onGroupedItemRemoved.ordinal()]);
+
+        gl.addAll("px","py","qx");
+        assertEquals(4,gl.getHeaders().size());   }
 }
